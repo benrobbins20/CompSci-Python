6304145b4b537d26d3cf6574e1b9496741ac8489
benjaminrobbins <benro2211@gmail.com>
2022-08-22 20:42:41 -0600

    hard is shit powerset walkthrough

--- powerset_recursion.py
+++ powerset_recursion.py
@@ -1,31 +1,41 @@
+
+# very hard to understand but follow the temp_cmbo after every step 
+
 def power_set(my_list):
-    # base case: an empty list
+    print("\n\nstart#################")
     if len(my_list) == 0:
         return [[]]
     # recursive step: subsets without first element
     print("List before recursion: {}".format(my_list))
     if len(my_list) == 1:
-        print('Length of 1!!!, power_set([1:]) == Empty list: {}'.format(power_set(my_list[1:])))
+        print('list length: 1, Proceeding...') 
     power_set_without_first = power_set(my_list[1:])  # why does it move forward when len(my_list) == 1??? because the value of power_set(my_list[1:]) RETURNS [] Therefore the recursion loop stops, and then will continue to next line 
-    print('###################')
-    print(power_set_without_first) # should be empty list (within main list)
+    print("$$$$$$$")
+    print("my_list full: {}".format(my_list))
+    print("Power set without first: {}".format(power_set_without_first)) # should be empty list (within main list)
+    print("current my_list[0]: {}".format(my_list[0]))
+    print("$$$$$$$")
     # subsets with first element
     # should still be a value at my_list[0]
     with_first = [[my_list[0]] + rest for rest in power_set_without_first]
-    print("with_first list: {}, Length of list: {}".format(with_first,len(with_first)))
     print()
-    print("without_first list: {} length of list: {}".format(power_set_without_first,len(power_set_without_first)))
+    print("with_first list: {}".format(with_first))
+    print()
+    print("without_first list: {}".format(power_set_without_first))
     print()
     # return combination of the two
-    print('end of function, returning')
-    print(with_first)
-    print(power_set_without_first)
+    temp_combo = (with_first + power_set_without_first)
+    print("current with_first: {}".format(with_first))
+    print("current without first: {}".format(power_set_without_first))
+    print(temp_combo)
+    print("end#################\n\n")
+    
     return with_first + power_set_without_first
   
 universities = ['A','B','C']
 
 power_set_of_universities = power_set(universities)
 
-#for set in power_set_of_universities:
-  #print(set)
-print(power_set_of_universities)
+for set in power_set_of_universities:
+  print(set)
+#print(power_set_of_universities)
