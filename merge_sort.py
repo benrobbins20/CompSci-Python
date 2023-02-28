340667ffde42e916b417a87a774f460fff248c87
BENSULTRARYZEN <benro2211@gmail.com>
2023-02-28 08:33:57 -0700

    merge sort

---
+++ merge_sort.py
@@ -0,0 +1,37 @@
+def merge_sort(items):
+  if len(items) <= 1:
+    return items
+  # leave this check here
+  
+
+  middle_index = len(items) // 2
+  left_split = items[:middle_index]
+  right_split = items[middle_index:]
+  
+  left_sorted = merge_sort(left_split)
+  right_sorted = merge_sort(right_split)
+
+  return merge(left_sorted,right_sorted)
+
+def merge(left, right):
+  result = []
+
+  while (left and right):
+    if left[0] < right[0]:
+      result.append(left[0])
+      left.pop(0)
+    else:
+      result.append(right[0])
+      right.pop(0)
+
+  if left:
+    result += left
+  if right:
+    result += right
+
+  return result
+
+
+check_list = [2,54,234,4,12,77,69,420]
+check = merge_sort(check_list)
+print(check)
