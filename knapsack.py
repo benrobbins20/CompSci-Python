a4f4129ee0c2f1b3f5ae84b7d5d4e73c701662e5
Benjamin Robbins <benro2211@gmail.com>
2023-06-24 00:17:19 -0600

    knapsack problem

---
+++ knapsack.py
@@ -0,0 +1,50 @@
+###################
+#
+# knapsack problem
+# made a nice diagram of a length of 4 list
+# shows the flow like the fib sequence and path through maximum knapsack value
+
+def knapsack_recursion(weight_cap,values,weights,n):
+    # max carrying cap
+    # list of values
+    # list of weights
+    # n is index
+    # for recursion, index will start from that last item
+    # base case will be if the carrying capacity is met (weight_cap == 0) or index == 0
+    
+    if weight_cap == 0 or n == 0:
+        # this will have already added the value of 0th index 
+        # can just return 0 and exit recursion loop
+        return 0
+    
+    elif weights[n-1] > weight_cap:
+        # weight exceeds weight cap 
+        # skip by recursive call to next index
+        knapsack_recursion(weight_cap, values, weights, n - 1)
+        
+    else:
+        # enter the recursive loop
+        # function call return max value between if that item is included or not
+        # if item is included, add value of item to return value, subtract weight of that item from the weight_cap, move to next item n - 1
+        # else move on to the next item n - 1
+        # with recursion the index will hit every index value, either add item or skip item
+        
+        nth_value = values[n - 1]
+        include_item = nth_value = knapsack_recursion(weight_cap - weights[n - 1], values, weights, n - 1)
+        exclude_item = knapsack_recursion(weight_cap, values, weights, n - 1)
+        
+        return max(include_item,exclude_item)
+
+weight_cap = 50    
+values = [90, 60, 100, 120]
+weights = [10, 10, 20, 30]
+if len(values) == len(weights):
+    n = len(values)
+else:
+    # silent exit
+    exit()
+max_ = knapsack_recursion(weight_cap,values,weights,n)
+print(max_)    
+    
+    
+    
