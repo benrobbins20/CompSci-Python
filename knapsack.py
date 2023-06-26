76e337f33025bd422076ccdd1df6a6af96f0f532
BENSULTRARYZEN <benro2211@gmail.com>
2023-06-25 23:08:59 -0600

    figured out dynamic

--- knapsack.py
+++ knapsack.py
@@ -3,7 +3,7 @@
 # made a nice diagram of a length of 4 list
 # shows the flow like the fib sequence and path through maximum knapsack value
 
-def knapsack_recursion(weight_cap,values,weights,n):
+def recursive_knapsack(weight_cap,values,weights,n):
     # max carrying cap
     # list of values
     # list of weights
@@ -37,13 +37,59 @@ def knapsack_recursion(weight_cap,values,weights,n):
         exclude_item = knapsack_recursion(weight_cap, values, weights, n - 1)
         return max(include_item,exclude_item)
 
+
+def dynamic_knapsack(weight_cap, weights, values):
+  rows = len(weights) + 1
+  cols = weight_cap + 1
+  # Set up 2D array
+  matrix = [ [] for x in range(rows) ]
+
+  # Iterate through every row
+  for index in range(rows):
+    # Initialize columns for this row
+    matrix[index] = [ -1 for y in range(cols) ]
+
+    # Iterate through every column
+    for weight in range(cols):
+      # Write your code here
+      if index == 0 or weight == 0:
+        # base case
+        matrix[index][weight] = 0
+        
+      elif weights[index - 1] <= weight:
+          # set the cell value = to the max between including or excluding item
+          # 
+          matrix[index][weight] = max(
+              values[index - 1] + matrix[index - 1][weight - weights[index - 1]], 
+              matrix[index - 1][weight],
+              )
+      else:
+        # if we exceed the weight limit on the item then it means we have to exclude it by default
+        # excluded items will be located one cell directly above over-weight cell 
+        matrix[index][weight] = matrix[index - 1][weight]
+        
+
+  
+  # Return the value of the bottom right of matrix
+  for row in matrix:
+    print(row)
+    #print("\n")
+  return matrix[rows-1][weight_cap]
+
+#####RECURSIVE KNAPSACK#########
+
 weight_cap = 50    
 values = [90, 60, 100, 120]
 weights = [10, 10, 20, 30]
 n = len(values)
+#max_ = knapsack_recursion(weight_cap,values,weights,n)
+#print(max_)    
 
-max_ = knapsack_recursion(weight_cap,values,weights,n)
-print(max_)    
-    
+
+#####DYNAMIC KNAPSACK#########
+weight_cap = 50
+weights = [31, 10, 20, 19, 4, 3, 6]
+values = [70, 20, 39, 37, 7, 5, 10]
+print(dynamic_knapsack(weight_cap, weights, values))    
     
     
